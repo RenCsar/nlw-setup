9dac3753deb047d4791da77451cdaa2233e0b027
Renan Cesar <cesar.renan@hotmail.com>
2023-01-17 12:13:02 -0300

    Configuração básica da versão WEB

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import "./styles/global.css";
+
+function App() {
+
+  return (
+    <>
+      <h1>Olá, mundo! Com Vite!</h1>
+      <div className='flex bg-zinc-400 items-center justify-center'>
+        <p>Com Tailwind</p>
+      </div>
+    </>
+  )
+}
+
+export default App
